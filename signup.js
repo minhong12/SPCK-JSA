b2e9ffa798234cc04bdbda871c69ac4f40ea9aa8
Quang Minh <ongquangminh2711@gmail.com>
2023-03-02 22:42:27 +0700

    Add login and signup forms

--- signup.js
+++ signup.js
@@ -3,13 +3,35 @@ const validata =()=>{
     var _email = document.signup.email.value;
     var _num = document.signup.number.value;
     var _pass = document.signup.password.value;
+    var ok = true;
 
     // Biểu thức chính quy
-    var checkpass = /"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"/;
-    var checkemail = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
-    var checknumber = /^\d+$/;
+    // var checkpass = /^([A-Z]){1}([\w_\.!@#$%^&*()]+){5,31}$/;
+    var checkemail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    var checknumber =/^0[0-9]{9,11}$/;
 
-    
+    if (!_email.match(checkemail)){
+        document.getElementById("email").style.display = "block";
+        ok =false;
+    } else {
+        document.getElementById("email").style.display = "none";
+    }
 
+    if (!_num.match(checknumber)){
+        document.getElementById("number").style.display = "block";
+        ok =false;
+    } else {
+        document.getElementById("number").style.display = "none";
+    }
 
+    // if (!_pass.match(checkpass)){
+    //     document.getElementById("password").style.display = "block";
+    //     ok =false;
+    // } else {
+    //     document.getElementById("password").style.display = "none";
+    // }
+
+    if (ok) {
+        return ok;
+    } else {return ok};
 }
