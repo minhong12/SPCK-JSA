c0c4e8c8964c352dfbdd0879c35c59a347b30810
Quang Minh <ongquangminh2711@gmail.com>
2023-03-04 18:50:54 +0700

    Update validation

--- signup.js
+++ signup.js
@@ -6,7 +6,7 @@ const validata =()=>{
     var ok = true;
 
     // Biểu thức chính quy
-    // var checkpass = /^([A-Z]){1}([\w_\.!@#$%^&*()]+){5,31}$/;
+    var checkpass = /^([a-z]){1}([\w_\.!@#$%^&*()]+){5,31}$/;
     var checkemail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
     var checknumber =/^0[0-9]{9,11}$/;
 
@@ -24,12 +24,12 @@ const validata =()=>{
         document.getElementById("number").style.display = "none";
     }
 
-    // if (!_pass.match(checkpass)){
-    //     document.getElementById("password").style.display = "block";
-    //     ok =false;
-    // } else {
-    //     document.getElementById("password").style.display = "none";
-    // }
+    if (!_pass.match(checkpass)){
+        document.getElementById("password").style.display = "block";
+        ok =false;
+    } else {
+        document.getElementById("password").style.display = "none";
+    }
 
     if (ok) {
         return ok;
